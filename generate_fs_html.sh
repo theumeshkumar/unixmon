04bb944ccf496a87e45ccbae5e83860d666ffbc2
theumeshkumar <121493277+theumeshkumar@users.noreply.github.com>
2022-12-27 20:38:33 +0530

    generate_html_update

---
+++ generate_fs_html.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+OUTPUT=fs.html
+echo "<!DOCTYPE HTML>" >> $OUTPUT
+echo "<HTML>" >> $OUTPUT
+echo "<HEAD>" >> $OUTPUT
+echo "<TITLE>fs utilization</TITLE>" >> $OUTPUT
+echo "</HEAD>" >> $OUTPUT
+echo "<body>" >> $OUTPUT
+cat hc.log | grep filesystem >> $OUTPUT
+echo "</body>" >> $OUTPUT
+echo "</HTML>" >> $OUTPUT
